4e902daf3033004d4bd745f4348ed56aa7396051
Srujan GV <srujangv05@gmail.com>
2022-08-24 19:47:39 +0530

    learnt loops and slices

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cards := []string{"Ace of spades", newCard()}
+	cards = append(cards, "King of hearts")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Six of diamonds"
 }
